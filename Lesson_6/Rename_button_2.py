76053a7de1eee2cddcbca00010bf17be4f94677c
Ворошилова_Светлана <vrshilova-s@mail.ru>
2024-04-19 19:55:17 +0300

    HW 6.1

--- Lesson_6/Rename_button_2.py
+++ Lesson_6/Rename_button_2.py
@@ -1,24 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+from webdriver_manager.chrome import ChromeDriverManager
 
-driver.get(" http://uitestingplayground.com/textinput") # Перейдите на страницу
-
-input_field = driver.find_element(By.CSS_SELECTOR, "#newButtonName")#Указать в поле ввода текст SkyPro
-input_field.send_keys("SkyPro")
-
-driver.find_element(By.CSS_SELECTOR, "#updatingButton").click()# Нажать на синюю кнопку
-
+driver = webdriver.Chrome(service=ChromeService
+                          (ChromeDriverManager().install()))
+# Перейдите на страницу
+driver.get(" http://uitestingplayground.com/textinput")
+# Указать в поле ввода текст SkyPro
+input_field = driver.find_element(By.CSS_SELECTOR, "#newButtonName").send_keys("SkyPro")
+# Нажать на синюю кнопку
+driver.find_element(By.CSS_SELECTOR, "#updatingButton").click()
+# Получить текст кнопки
 button_text = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.CSS_SELECTOR, ".btn-primary")) # Получить текст кнопки
-).text
-
-print(button_text) # Вывести в консоль (SkyPro)
+    EC.visibility_of_element_located
+    ((By.CSS_SELECTOR, ".btn-primary"))).text
+# Вывести в консоль (SkyPro)
+print(button_text) 
 
 driver.quit()
