3a9b6e1f02d1beb41c97a0c7c93c887da8f280ea
Ворошилова_Светлана <vrshilova-s@mail.ru>
2024-05-10 16:00:29 +0300

    add seventh homework

--- Lesson_8/EmployeesApi.py
+++ Lesson_8/EmployeesApi.py
@@ -3,30 +3,7 @@ import requests
 class EmployeesApi: 
     def __init__(self, url):
         self.url = url
-
-    def get_company(self, id):
-        resp = requests.get(self.url + '/company/' + str(id))
-        return resp.json()
-
-    def get_employee(self,id):
-        resp = requests.get(self.url + '/employee/' + str(id))
-        return resp.json() 
-
-    def get_employees_list(self,companyId, params_to_add=None):
-        params = {'companyId': companyId}
-        if params_to_add:
-           params.update(params_to_add)
-        resp = requests.get(self.url + '/employee', params=params)
-        return resp.json()
-
-    def get_token(self, user = 'bloom', password = 'fire-fairy'):
-        creds = {
-           'username': user,
-           'password': password
-           }
-        resp = requests.post(self.url+'/auth/login', json=creds)
-        return resp.json()['userToken'] 
-
+        
     def create_company(self, name, description=""):
         company = {
             "name": name,
@@ -35,7 +12,35 @@ class EmployeesApi:
         my_headers = {}
         my_headers["x-client-token"] = self.get_token()
         resp = requests.post(self.url + '/company', json=company, headers=my_headers)
+        print(resp)
         return resp.json()
+
+    def get_company(self, id):
+        resp = requests.get(self.url + '/company/' + str(id))
+        print(resp)
+        return resp.json()
+
+    def get_employee(self,id):
+        resp = requests.get(self.url + '/employee/' + str(id))
+        print(resp)
+        return resp.json() 
+
+    def get_employees_list(self,companyId, params_to_add=None):
+        params = {'companyId': companyId}
+        if params_to_add:
+           params.update(params_to_add)
+        resp = requests.get(self.url + '/employee', params=params)
+        print(resp)
+        return resp.json()
+
+    def get_token(self, user = 'bloom', password = 'fire-fairy'):
+        creds = {
+           'username': user,
+           'password': password
+           }
+        resp = requests.post(self.url+'/auth/login', json=creds)
+        print(resp)
+        return resp.json()['userToken'] 
 
     def create_employee(self, firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive):
         employee = {
@@ -52,21 +57,19 @@ class EmployeesApi:
         my_headers = {}
         my_headers["x-client-token"] = self.get_token()
         resp = requests.post(self.url + '/employee', json=employee, headers=my_headers)
-        if resp.status_code == 200:
-            return resp.json()
-        else:
-             return None
+        print(resp)
+        return resp.json()
 
-    def edit_employee(self, new_firstName, new_lastName, new_email, new_phone, new_url, new_isActive):
-        my_headers = {}
-        my_headers["x-client-token"] = self.get_token()
+    def edit_employee(self, new_lastName, new_email, new_phone, new_url, new_isActive):
         employee = {
-           "firstName":new_firstName,
-           "lastName": new_firstName,
+           "lastName": new_lastName,
            "email": new_email,
            "url":  new_url,
            "phone": new_phone,
            "isActive": new_isActive
            }
-        resp = requests.patch(self.url + '/employee/' + str(new_id), headers=my_headers, json=company)
+        my_headers = {}
+        my_headers["x-client-token"] = self.get_token()   
+        resp = requests.patch(self.url + '/employee/' + str(id), headers=my_headers, json=employee)
+        print(resp)
         return resp.json()      
