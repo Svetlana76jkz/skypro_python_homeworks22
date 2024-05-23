a80e4f9b30917e216f24ff142908e21ee51c7cce
Ворошилова_Светлана <vrshilova-s@mail.ru>
2024-05-23 20:34:48 +0300

    Bcghf

--- Lesson_9/EmployeesTable.py
+++ Lesson_9/EmployeesTable.py
@@ -2,43 +2,40 @@ from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 
 class EmployeesTable:
-    scripts = {
-       "insert new company": text("insert into company (name, description) values (:new_name, :new_description)"),
-        "get max id company": text("select MAX(id) AS max_id from company"),
-        "select": text("select * from employee where id = : companyId"),
-        "insert new employee": text("insert into employee (first_name, last_name, middle_name, phone, email, birthdate, avatar_url, company_id) values (:new_firstName, :new_lastName, :new_middleName, :new_companyId, :new_email, :new_url, :new_phone, :new_birthdate, :new_isActive)"),
-        "get max id empl": text("select MAX(id) AS max_id from employee"),
-        "edited employee": text("update employee set last_name = :new_lastName, email = :new_email, url = :new_url, phone = :new_phone, isActive = :new_isActive where id = :employeeId"),
-        "delete id employee": text("delete from employee where id = : id_to_delete"),
-        "delete by id": text("delete from company where id = : id_to_delete")
-    }
+    __scripts = {
+        "insert company": text("insert into company (\"name\", \"description\") values (:new_name, :new_descr)"),
+        "get max id company": text("select MAX(id) from company"),
+        "select": text("select * from employee where company_id = :id"),
+        "insert new employee": text("insert into employee (first_name, last_name, middle_name, phone, email, birthdate, avatar_url, is_active, company_id) values (:first_name, :last_name, :middle_name, :phone, :email, :birthdate, :avatar_url, :is_active, :company_id)"),
+        "get max id empl": text("select MAX(id) from employee"),
+        'edited employee': text("update employee set last_name = :new_lastName, email = :new_email, url = :new_url, phone = :new_phone, isActive = :new_isActive where id = :employeeId"),
+        'delete id employee': text("delete from employee where id = :id_to_delete"),
+        "delete by id": text("delete from company where id = :id_to_delete")
+         }
 
     def __init__(self, connection_string):
         self.__db = create_engine(connection_string)
 
     def create_company_db(self, name, description):
-        return self.__db.execute(self.__scripts["insert new company"], new_name = name, new_description = description)
+        self.__db.execute(self.__scripts["insert company"], new_name = name, new_descr = description)
 
     def get_max_id_company(self):
         return self.__db.execute(self.__scripts["get max id company"]).fetchall()[0][0]
 
+    def get_emploees_db(self, company_id):
+        return self.__db.execute(self.__scripts["select"], id = company_id).fetchall()
+
     def delete(self, id):
-        return self.__db.execute(self.__scripts["delete by id"], id_to_delete = id)     
+        return self.__db.execute(self.__scripts["delete by id"], id_to_delete = id)
 
-    def get_emploees_db(self):
-        return self.__db.execute(self.__scripts["select"]).fetchall()    
-
-    def create_employee_db(self, firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive):
-        return self.__db.execute(self.scripts["insert new employee"], new_firstName=firstName, new_lastName=lastName, new_middleName=middleName, new_companyId=companyId, new_email=email, new_url=url, new_phone=phone, new_birthdate=birthdate, new_isActive=isActive)
+    def create_employee_db(self, first_name, last_name, middle_name, phone, email, birthdate, avatar_url, company_id, is_active):
+        self.__db.execute(self.__scripts["insert new employee"], first_name = first_name, last_name = last_name, middle_name = middle_name, phone = phone, email = email, birthdate = birthdate, avatar_url =avatar_url, company_id = company_id, is_active = is_active)
 
     def get_max_id_employee(self):
-        return self.__db.execute(self.__scripts["get max id empl"]).fetchall()[0][0]
+        return self.__db.execute(self.__scripts['get max id empl']).fetchall()[0][0]
 
     def update_employee(self, employeeId, new_lastName, new_email, new_url, new_phone, new_isActive):
-        return self.__db.execute(self.scripts["edited employee"], employeeId=employeeId, new_lastName=new_lastName, new_email=new_email, new_url=new_url, new_phone=new_phone, new_isActive=new_isActive)
+        return self.__db.execute(self.scripts['edited employee'], employeeId=employeeId, new_lastName=new_lastName, new_email=new_email, new_url=new_url, new_phone=new_phone, new_isActive=new_isActive)
 
-    def delete_employee_db():
-         return self.__db.execute(self.__scripts["delete id employee"], id_to_delete = id)
-
-        
-
+    def delete_employee_db(self, id):
+        return self.__db.execute(self.__scripts['delete id employee'], id_to_delete = id)
--- Lesson_9/db_employees_test.py
+++ Lesson_9/db_employees_test.py
@@ -1,139 +1,198 @@
-import requests
 import random
 from faker import Faker
 from EmployeesApi import EmployeesApi
 from EmployeesTable import EmployeesTable
 
 api = EmployeesApi("https://x-clients-be.onrender.com")
-db = EmployeesTable("postgres://x_clients_user:x7ngHjC1h08a85bELNifgKmqZa8KIR40@dpg-cn1542en7f5s73fdrigg-a.frankfurt-postgres.render.com/x_clients_xxet")
+db = EmployeesTable("postgresql://x_clients_db_3fmx_user:mzoTw2Vp4Ox4NQH0XKN3KumdyAYE31uq@dpg-cour99g21fec73bsgvug-a.oregon-postgres.render.com/x_clients_db_3fmx")
 
-fake = Faker()
+fake = Faker("ru_RU")
 
-def generate_random_employee_data():
-    firstName = fake.first_name()
-    lastName = fake.last_name()
-    middleName = fake.middle_name()
-    email = fake.email()
-    url = fake.url()
-    phone = fake.phone_number()
-    birthdate = fake.date_of_birth(minimum_age=18,maximum_age=65)
-    isActive = random.choice([True, False])
-    return firstName, lastName, middleName, email, url, phone, birthdate, isActive
-
-def generate_random_company():
+def generate_company():
     name = fake.name()
-    description =fake.descr()
+    description = fake.text(max_nb_chars=20)
     return name, description
 
-def generate_random_update_employee():
+def generate_employee(companyId):
+    firstName = fake.first_name()
+    lastName = fake.last_name()
+    middleName = fake.first_name() + "овна"
+    phone = fake.random_number(digits = 11, fix_len = True)
+    email = fake.email(domain = "example.com")
+    birthdate = '1976-07-02'
+    url = fake.url(schemes = ['http', 'https'])
+    isActive = random.choice([True, False])
+    return firstName, lastName, middleName, phone, email, birthdate, url, isActive
+
+def generate_edit_employee(companyId):
     new_lastName = fake.last_name()
-    new_email = fake.email()
-    new_url = fake.url()
-    new_phone = fake.phone_number()
-    new_isActive = fake.boolean()
-    return new_lastName, new_email, new_url, new_phone, new_isActive    
-    
-def test_get_list_employees():
-    # Создать новую компанию
-    name, description = generate_random_company()
+    new_email = fake.email(domain = "example.com")
+    new_url = fake.url(schemes = ['http', 'https'])
+    new_phone = fake.random_number(digits = 11, fix_len = True)
+    new_isActive = random.choice([True, False])
+    return new_lastName, new_email, new_url, new_phone, new_isActive
+
+def test_get_list_employee():
+    name, description = generate_company()
     db.create_company_db(name, description)
     max_id = db.get_max_id_company()
 
-    # Длина списка сотрудников в api и бд
-    api_result = api.get_employees_list(companyId)
-    db_result = db.get_emploees_db()
-    
-    db.delete(max_id)
+    new_company = api.get_company(max_id)
 
-    assert len(api_result) == len(db_result)
+    api_list = api.get_employees_list(max_id)
+    db_list = db.get_emploees_db(max_id)
+
+    assert len(api_list) == len(db_list)
 
 def test_add_new_employee():
-    # Создать новую компанию
-    name, description = generate_random_company()
+    name, description = generate_company()
     db.create_company_db(name, description)
     max_id = db.get_max_id_company()
 
-    # Обратиься к компании по id
     new_company = api.get_company(max_id)
-    companyId = new_company["id"]
 
-    # Длина списка сотрудников в api и бд до...
-    api_result_before = api.get_employees_list(companyId)
-    db_result_before = db.get_emploees_db()
-    
-    # добавить нового сотрудника
-    firstName, lastName, middleName, email, url, phone, birthdate, isActive = generate_random_employee_data()
-    db.create_employee_db(firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive)
-    max_emp_id = db.get_max_id_employee()
+    api_list_before = api.get_employees_list(max_id)
+    db_list_before = db.get_emploees_db(max_id)
 
-    # Длина списка сотрудников  в api и бд после...
-    api_result_after = api.get_employees_list(companyId)
-    db_result_after = db.get_emploees_db()
+    firstName, lastName, middleName, phone, email, birthdate, url, isActive = generate_employee(max_id)
+    db.create_employee_db(firstName, lastName, middleName, phone, email, birthdate, url, max_id, isActive)
+    max_id_empl = db.get_max_id_employee()
 
-    # Обращаемся к сотруднику по iD
-    new_employee = api.get_employee(max_emp_id)
-    employee_id = new_employee["id"]
+    api_list_after = api.get_employees_list(max_id)
+    db_list_after = db.get_emploees_db(max_id)
+
+    db.delete_employee_db(max_id_empl)
 
     db.delete(max_id)
 
-    assert  len(api_result_before) + 1 == len(api_result_after)
-    assert len(db_result_before) + 1 == len(db_result_after)
-    assert len(api_result_after) == len(db_result_after)
-    assert new_employee [-1]["firstName"] == first_name
-    assert new_employee [-1]["lastName"] == last_name
-    assert new_employee [-1]["middleName"] == middle_name
-    assert new_employee [-1]["companyId"] == companyId
-    assert new_employee [-1]["phone"] == phone_number
-    assert new_employee [-1]["birthdate"] == date_of_birth
-    assert new_employee [-1]["isActive"] == True
-    assert new_employee [-1]["id"] == max_emp_id
+    assert len(api_list_before) == len(db_list_before)
+    assert len(api_list_after) == len(db_list_after)
+    assert len(api_list_after) - len(api_list_before) == 1
+    assert len(db_list_after) - len(db_list_before) == 1
+    assert api_list_after[-1]['firstName'] == firstName
+    assert api_list_after[-1]['lastName'] == lastName
+    assert api_list_after[-1]['middleName'] == middleName
+    #assert api_list_after[-1]['phone'] == phone
+    assert api_list_after[-1]['email'] == email
+    assert api_list_after[-1]['birthdate'] == birthdate
+    assert api_list_after[-1]['avatar_url'] == url
+    assert api_list_after[-1]['isActive'] == isActive
+    assert api_list_after[-1]['id'] == max_id_empl
+
+def test_get_one_employee():
+    name, description = generate_company()
+    db.create_company_db(name, description)
+    max_id = db.get_max_id_company()
+
+    new_company = api.get_company(max_id)
+
+    api_list_before = api.get_employees_list(max_id)
+    db_list_before = db.get_emploees_db(max_id)
+
+    firstName, lastName, middleName, phone, email, birthdate, url, isActive = generate_employee(max_id)
+    db.create_employee_db(firstName, lastName, middleName, phone, email, birthdate, url, max_id, isActive)
+    max_id_empl = db.get_max_id_employee()
+
+    api_list_after = api.get_employees_list(max_id)
+    db_list_after = db.get_emploees_db(max_id)
+
+    new_employee = api.get_employee(max_id_empl)
+
+    db.delete_employee_db(max_id_empl)
+
+    db.delete(max_id)
+
+    assert len(api_list_before) == len(db_list_before)
+    assert len(api_list_after) == len(db_list_after)
+    assert len(api_list_after) - len(api_list_before) == 1
+    assert len(db_list_after) - len(db_list_before) == 1
+    assert new_employee["firstName"] == firstName
+    assert  new_employee["lastName"] ==  lastName
+    assert new_employee["middleName"] == middleName
+    assert new_employee["companyId"] == max_id
+    #assert new_employee["phone"] ==  phone
+    assert new_employee['avatar_url'] == url
+    assert new_employee["birthdate"] == birthdate
+    assert new_employee["isActive"] == True
+    assert new_employee["id"] == max_id_empl
 
 def test_patch_employee():
-     # Создать новую компанию
-    name, description = generate_random_company()
+    name, description = generate_company()
     db.create_company_db(name, description)
     max_id = db.get_max_id_company()
 
-    # Обратиься к компании по id
     new_company = api.get_company(max_id)
-    companyId = new_company["id"]
 
-    # добавить нового сотрудника
-    firstName, lastName, middleName, email, url, phone, birthdate, isActive = generate_random_employee_data()
-    db.create_employee_db(firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive)
-    max_emp_id = db.get_max_id_employee()
+    api_list_before = api.get_employees_list(max_id)
+    db_list_before = db.get_emploees_db(max_id)
 
-    # Изменить информацию по сотруднику
-    new_lastName, new_email, new_url, new_phone, new_isActive = generate_random_update_employee
-    db.update_employee(new_lastName, new_email, new_url, new_phone, new_isActive)
+    firstName, lastName, middleName, phone, email, birthdate, url, isActive = generate_employee(max_id)
+    db.create_employee_db(firstName, lastName, middleName, phone, email, birthdate, url, max_id, isActive)
+    max_id_empl = db.get_max_id_employee()
+
+    api_list_after = api.get_employees_list(max_id)
+    db_list_after = db.get_emploees_db(max_id)
+
+    new_lastName, new_email, new_url, new_phone, new_isActive = generate_edit_employee(max_id)
+    edited = api.edit_employee(new_lastName, new_email, new_url, new_phone, new_isActive, max_id_empl)
+
+    db.delete_employee_db(max_id_empl)
 
     db.delete(max_id)
-    
-    assert len(api_result) == len(db_result) 
-    assert edited["email"] == new_email
+
     assert edited["url"] == new_url
-    assert edited["isActive"] == new_isActive    
+    assert edited["isActive"] == new_isActive
+    assert edited["email"] == new_email
+    #assert edited["phone"] == new_phone
+    #assert edited["lastName"] == new_lastName
 
 def test_delete_employee():
-     # Создать новую компанию
-    name, description = generate_random_company()
+    name, description = generate_company()
     db.create_company_db(name, description)
     max_id = db.get_max_id_company()
 
-    # Обратиься к компании по id
     new_company = api.get_company(max_id)
-    companyId = new_company["id"]
 
-    # добавить нового сотрудника
-    firstName, lastName, middleName, email, url, phone, birthdate, isActive = generate_random_employee_data()
-    db.create_employee_db(firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive)
-    max_emp_id = db.get_max_id_employee()
-    
-    # удалить сотрудника
-    del_emp = api.delete_employee(max_emp_id)
-    db.delete_employee_db(max_emp_id)
+    api_list_before = api.get_employees_list(max_id)
+    db_list_before = db.get_emploees_db(max_id)
+
+    firstName, lastName, middleName, phone, email, birthdate, url, isActive = generate_employee(max_id)
+    db.create_employee_db(firstName, lastName, middleName, phone, email, birthdate, url, max_id, isActive)
+    max_id_empl = db.get_max_id_employee()
+
+    api_list_after = api.get_employees_list(max_id)
+    db_list_after = db.get_emploees_db(max_id)
+
+    db.delete_employee_db(max_id_empl)
 
     db.delete(max_id)
 
-    # Проверить, что сотрудник был удален
-    assert del_emp is not None, "Сотрудник не был удален"       
+    assert not db.get_emploees_db(max_id)
+
+def test_add_del_several_empl():
+    name, description = generate_company()
+    db.create_company_db(name, description)
+    max_id = db.get_max_id_company()
+
+    new_company = api.get_company(max_id)
+
+    api_list_before = api.get_employees_list(max_id)
+    db_list_before = db.get_emploees_db(max_id)
+
+    for i in range(10):
+        firstName, lastName, middleName, phone, email, birthdate, url, isActive = generate_employee(max_id)
+        db.create_employee_db(firstName, lastName, middleName, phone, email, birthdate, url, max_id, isActive)
+
+    max_id_empl = db.get_max_id_employee()
+
+    api_list_after = api.get_employees_list(max_id)
+    db_list_after = db.get_emploees_db(max_id)
+
+    for i in range(10):
+        db.delete_employee_db(max_id_empl - i)
+
+    db.delete(max_id)
+
+    assert len(api_list_after) - len(api_list_before) == 10
+    assert len(db_list_after) - len(db_list_before) == 10
+    assert not db.get_emploees_db(max_id)
